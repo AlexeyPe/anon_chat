f20634cf054cd9323d0b63acc0ba2bb48edf0269
Alexey Pechenckin <pechenckinalexey@gmail.com>
2024-09-26 18:04:43 +0500

    form textarea ввод сообщения

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,45 @@
+<script lang="ts">
+	import CornerBottomRight from "svelte-radix/CornerBottomRight.svelte";
+
+	import { Textarea } from "$lib/components/ui/textarea";
+	import { Label } from "$lib/components/ui/label";
+	import { Button } from "$lib/components/ui/button/index.js";
+
+	let maxLength:number = 60
+	let currentLength:number = 0
+	let currentMessage:string
+	let textError:string = ""
+	const textAreaKeyup = () => {
+		currentLength = currentMessage.length
+		textError = currentLength == maxLength ? "text-error" : ""
+	}
+</script>
+
 <svelte:head>
 	<title>Anon Chat</title>
 	<meta name="description" content="Anon Chat site" />
 </svelte:head>
 
 <div class="h-full flex flex-col justify-end pb-4">
-	
-	<h1>Hello world!</h1>
+	<form class="bg-background focus-within:ring-ring relative overflow-hidden rounded-lg border focus-within:ring-1">
+		<Label for="message" class="sr-only">сообщение</Label>
+		<Textarea
+			required
+			id="message"
+			placeholder="Введите ваше сообщение здесь"
+			bind:value={currentMessage}
+			on:input={textAreaKeyup}
+			maxlength={maxLength}
+			class="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+		/>
+
+		<div class="flex justify-between p-3 pt-0">
+			<span class="text-sm self-center {textError}">{currentLength}/{maxLength}</span>
+			<Button type="submit" size="sm" class="gap-1">
+				Отправить
+				<CornerBottomRight class="size-4"/>
+			</Button>
+		</div>
+	</form>
 </div>
 
